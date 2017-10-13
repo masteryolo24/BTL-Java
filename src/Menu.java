0c5f708688a6632e4ffb4ad2a890e0592d6feca7
nmc1998 <nmc7198@gmail.com>
2017-10-13 18:39:09 +0700

    update menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,17 +1,27 @@
+import javax.sound.sampled.Line;
 import javax.swing.*;
- class menu {
-    JFrame f;
-    menu() {
-        f = new JFrame(); // Tao cua so cho app
 
-        JButton b1 = new JButton("Analyze a class"); f.add(b1);
-        JButton b2 = new JButton("Class"); f.add(b2);
+public class menu extends JFrame {
+    JPanel p = new JPanel();
+    JButton b1 = new JButton("Read a class");
+    JButton b2 = new JButton("Exit");
 
-        b1.setBounds(130, 100, 300, 40);
-        b2.setBounds(130, 250, 100, 40);
+    public menu() {
+        super("Bài tập lớn");
+        setSize(800, 600);
+        p.setLayout(null);
 
-        f.setSize(800, 600);
-        f.setLayout(null); // K su dung Layout Manager
-        f.setVisible(true);
+        b1.setBounds(300, 200, 200, 40);
+        b2.setBounds(350, 270, 100, 40);
+        p.add(b1);
+        p.add(b2);
+        add(p);
+
+
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
     }
+
 }
