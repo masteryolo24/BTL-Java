352898f0b86ca107d0c13352ad74912798d3f077
Duc Vu Tran <mrtranmr24@gmail.com>
2017-12-04 13:48:58 +0700

    Update findClass.java

--- src/findClass.java
+++ src/findClass.java
@@ -16,8 +16,64 @@ class findClass{
         	int i2 = s.indexOf(" ", i+7);
         	String tmp = s.substring(i +7,  i2);
         	String tmp2 = s.substring(temp, i+1);
-        	if (tmp2.equals(" abstract ")) System.out.println("===Abstract Class===");
-        	else System.out.println("===Class===");
+        	if (tmp2.equals(" abstract ")) System.out.println("=== Abstract Class ===");
+        	else System.out.println("=== Class ===");
+        	System.out.println("+Name: " + tmp);
+        	int k1 = s.indexOf("{", i);
+        	for (int j=k1; j <s.length(); j++){
+    			if(j ==-1) break;
+    			if(count==0) {
+    			}
+    			if(s.charAt(j) == '{') {
+                    count++;
+                }
+    			if(s.charAt(j) == '}') {
+    		        count--;
+    		        if(count ==0) {
+    		            tmp3=j;	      
+    		            j = s.indexOf("{", tmp3)-1;
+                        str = s.substring(i, tmp3-1);
+                        if (str.indexOf(" class ", str.indexOf("class") +7)!=-1) {
+                        	int l = str.indexOf(" class ", str.indexOf("class") +7);
+                        	String str2 = str.substring(l-2, str.length()-2);
+                        	f.innerClass(str2, tmp);
+                        	i = s.indexOf(" class " , i+1);
+                        }
+                        if(str.indexOf(" extends ") !=-1) {
+                        	System.out.println("*" + tmp + " is a " + f.getExtend(str));
+                        }
+                        f.nameMethod(str);
+                        f.nameAttribute(str, s);
+                        if (f.hasImplement(str)) {
+                        	System.out.println("* " + tmp + " is a: ");
+	                        for (int x= 0; x<f.getImplement(str).length; x++) {
+	                        	System.out.println("   " +f.getImplement(str)[x].trim());
+	                        }
+                        }
+                        break;
+                    }
+                }
+            }
+        	i = s.indexOf(" class ", i+1) -1;
+        	
+        }
+    }
+    
+    public void innerClass (String s, String s2) {
+    	int count =0, tmp3;
+    	String str;
+    	findClass f = new findClass();
+    	//System.out.println("Class: ");
+        int i1 = s.indexOf(" class ");
+        //int i2;
+        for (int i=i1; i< s.length(); i++) {
+        	int temp = s.lastIndexOf(" ", i-1);
+        	if (i ==-1) break;
+        	int i2 = s.indexOf(" ", i+7);
+        	String tmp = s.substring(i +7,  i2);
+        	String tmp2 = s.substring(temp, i+1);
+        	if (tmp2.equals(" abstract ")) System.out.println("=== Abstract Class ===");
+        	else System.out.println("=== Inner Class " + s2 +"===");
         	System.out.println("+Name: " + tmp);
         	int k1 = s.indexOf("{", i);
         	for (int j=k1; j <s.length(); j++){
@@ -65,7 +121,7 @@ class findClass{
         	if (i ==-1) break;
         	int i2 = s.indexOf(" ", i+11);
         	String tmp = s.substring(i +11,  i2);
-        	System.out.println("===Interface===");
+        	System.out.println("=== Interface Class ===");
         	System.out.println("+Name: " + tmp);
         	int k1 = s.indexOf("{", i);
         	for (int j=k1; j <s.length(); j++){
@@ -159,7 +215,7 @@ class findClass{
         String s2 = new String();
         if(i3 != -1) {
             int i4= s.indexOf(";");
-            s2 = "Package's Name: "+s.substring(i3+9, i4);
+            s2 = "+ Package's Name: "+s.substring(i3+9, i4);
         }
         return s2;
     }
@@ -225,7 +281,7 @@ class findClass{
 		findClass f= new findClass();
 		int i2 = s.indexOf(";");
 		if (i2==-1)System.out.println("Don't have Attribute!");
-		else System.out.println("Attribute:");
+		//else System.out.println("Attribute:");
 		while (i2!=-1) {
 			int j =  s.lastIndexOf("new ", i2);
 			//int k = s.lastIndexOf("=", i2);
@@ -234,7 +290,8 @@ class findClass{
 			//System.out.println(k);
 			//System.out.println(l);
 
-			if(s.charAt(i2-2) == ')') {
+			if(s.charAt(i2-2) == ')' && s.charAt(s.lastIndexOf(" " , s.lastIndexOf(" ", s.lastIndexOf("(", i2))-1)-4) == '=') {
+				//System.out.print(s.charAt(s.lastIndexOf("=new" , s.lastIndexOf(" ", s.lastIndexOf(" ", i2)-1)-1)));
 				int k = s.lastIndexOf("=", i2);
 				int k2 = s.lastIndexOf(" ", s.lastIndexOf(" ", k)-1);
 				name = s.substring(k2+1, k);
@@ -264,6 +321,7 @@ class findClass{
 	}
 	
 	
+    
     public String getExtend (String s) {
     	String tmp;
     	int i = s.indexOf(" extends ");
@@ -293,7 +351,7 @@ class findClass{
 	public void nameMethod(String s) {
 		int i = s.indexOf("(");
 		if(i==-1) System.out.println("Don't have Method!");
-		else System.out.println("Method: ");
+		//else System.out.println("Method: ");
 		while (i!=-1) {
 			int tmp2 = s.lastIndexOf(" ", i);
 			int tmp3 = s.lastIndexOf(" ", tmp2-1);
@@ -305,15 +363,15 @@ class findClass{
 				if ((s.substring(tmp6+1, tmp5)).equals("abstract")){
 					//System.out.println("Hello World");
 					if((s.substring(s.lastIndexOf(" ", tmp6-1)+1, tmp6)).equals("private")) {
-						System.out.print("-");
+						System.out.print("+ -");
 						System.out.println(s.substring(tmp3+1, tmp7-1) + ": " +s.substring(tmp5+1, tmp3) + "  (Abstract Method)");
 					}
 					else if((s.substring(s.lastIndexOf(" ", tmp6-1)+1, tmp6)).equals("protected")) {
-						System.out.print("#");
+						System.out.print("+ #");
 						System.out.println(s.substring(tmp3+1, tmp7-1) + ": " +s.substring(tmp5+1, tmp3) + "  (Abstract Method)");
 					}
 					else { 
-						System.out.print("+");
+						System.out.print("+ +");
 						System.out.println(s.substring(tmp3+1, tmp7-1) + ": " +s.substring(tmp5+1, tmp3) + "  (Abstract Method)");
 					}
 					i= s.indexOf("(", i+1);
@@ -328,21 +386,21 @@ class findClass{
 				}
 				if((s.substring(tmp6+1, tmp5)).equals("static")) {
 					if((s.substring(s.lastIndexOf(" ", tmp6-1)+1, tmp6)).equals("private")) {
-						System.out.print("-");
+						System.out.print("+ -");
 					}
 					else if((s.substring(s.lastIndexOf(" ", tmp6-1)+1, tmp6)).equals("protected")) {
-						System.out.print("#");
+						System.out.print("+ #");
 					}
-					else System.out.print("+");
+					else System.out.print("+ +");
 				}
 				else {
 					if ((s.substring(tmp6+1, tmp5)).equals("private")) {
-						System.out.print("-");
+						System.out.print("+ -");
 					}
 					else if ((s.substring(tmp6+1, tmp5)).equals("protected")) {
-						System.out.print("#");
+						System.out.print("+ #");
 					}
-					else System.out.print("+");
+					else System.out.print("+ +");
 				}
 				System.out.println(s.substring(tmp3+1, tmp4-1) + ": " +s.substring(tmp5+1, tmp3));
 				i = s.indexOf("(", tmp4);
