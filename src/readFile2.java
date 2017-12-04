3088c5460498ec907e5a830c18867816e28b7da3
Duc Vu Tran <mrtranmr24@gmail.com>
2017-12-04 13:05:14 +0700

    Update readFile2.java

--- src/readFile2.java
+++ src/readFile2.java
@@ -1,9 +1,13 @@
+package BaiTapLon;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class readFile2 {
-    public String docFile(String FILENAME) {
+    private static final Object[] String = null;
+
+	public String docFile(String FILENAME) {
         String s = "";
         BufferedReader br = null;
         FileReader fr = null;
@@ -17,8 +21,9 @@ public class readFile2 {
             br = new BufferedReader(new FileReader(FILENAME));
 
             while ((currentLine = br.readLine()) != null) {
-                //bo 2 dong dau tien
-                s += currentLine + "\n";
+                currentLine = currentLine.trim();
+
+                s = s + currentLine + " ";
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -35,10 +40,73 @@ public class readFile2 {
                 ex.printStackTrace();
             }
         }
-        //s = f2.deleteString3(s);
-        // s = f2.deleteString4(s);
+        
+        //s = s.trim();
+        //s = s.replace("{", " {").replace("(", " (").replace(") ", ")").replace("\"", " \" ").replace(" ;", ";").replace(" = ", "=").replace("= ", "=").replace(" =", "=").replaceAll(";", " ;").replaceAll("\\s+", " ");
 
+       
         return s;
     }
+    
+    public int countClass(String s) {
+    	int count=0;
+    	int i= s.indexOf(" Class ");
+    	while (i!=-1) {
+    		count++;
+    		i= s.indexOf(" Class " , i+1);
+    	}
+    	return count;
+    }
+    
+    public String getClassName(String s) {
+    	String str = null;
+    	int k=0;
+    	int i = s.indexOf(" Class ");
+    		int j =s.indexOf("+Name: ", i);
+    		str =  s.substring(j, s.indexOf(" ", j+6));
+    	return str;
+    }
+    
+    public int countName(String s) {
+    	int count=0;
+    	int i= s.indexOf("+ ");
+    	while (i!=-1) {
+    		count++;
+    		i= s.indexOf("+ " , i+1);
+    	}
+    	return count;
+    }
+    
+    public String[] getName(String s) {
+    	String[] str = null;
+    	int i = s.indexOf("+ ");
+    	int count =1;
+    	while (i!=-1) {
+    		int j = s.indexOf("+ ", i+1);
+    		if (j==-1) {
+    			j = s.indexOf("=", i+1);
+    		}
+    		str[count] = s.substring(i+1, j-1);
+    		i = s.indexOf("+ ", i+1);
+    	}
+    	return str;
+    }
 
+    public String[][] getAll(String s ) {
+    	readFile2 r= new readFile2();
+    	String[][] getInfo = null;
+    	int count=0;
+    	int i = s.indexOf(" Class ");
+    	while (i!=-1) {
+    		int j = s.indexOf(" Class ", i+1);
+    		String tmp = s.substring(i, j-1);
+    		getInfo[count][0] = r.getClassName(tmp);
+    		for (int k = 1 ; k<=countName(s); k++) {
+    			getInfo[count][k] = r.getName(tmp+9)[k];
+    		}
+    		i = s.indexOf(" Class ", i+1);
+    		count++;
+    	}
+    	return getInfo;
+    }
 }
