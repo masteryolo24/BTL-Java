ebc08901a397ea25b86c619f2ff8d825b5d200e4
Duc Vu Tran <mrtranmr24@gmail.com>
2017-12-11 19:08:45 +0700

    Update test.java

--- src/test.java
+++ src/test.java
@@ -18,78 +18,48 @@ public class test {
     	
     	//readFile2 r3 = new readFile2();
     	this.tmp = r3.docFile("./diagram.txt");
-    	this.tmp2 =r3.docFile("./diagram.txt");
+    	this.tmp2 =r3.docFile2("./diagram.txt");
     	
-        this.numberClass = r3.countClass(tmp);
-        this.numberClassAttributes  = r3.numberAttribute(tmp);
-        this.maxNumberInfo = maxNumberInfo();
-        this.maxNumberClassInfo = maxNumberClassInfo();
+    	this.numberClass = r3.countClass(tmp);
+    	this.numberClassAttributes  = r3.numberAttribute(tmp);
+    	this.numberClassMethods = r3.numberMethod(tmp);
         
-        this.numberClassMethods = r3.numberMethod(tmp);
-        this.numberClassInfo= numberClassInfo();
-        this.longestStringLen = longestStringLen();
-        this.relationship = relationship();
-        this.getInfo = getInfo();
-    }
-
-    int maxNumberInfo() {
-        int maxNumberInfo = 0;
-        for(int i = 0; i < numberClass; i++) {
-            if(numberClassInfo()[i] > maxNumberInfo)
-                maxNumberInfo = numberClassInfo()[i];
-        }
-        return maxNumberInfo;
-    }
-    int maxNumberClassInfo() {
-        int maxNumberClassInfo = 0;
-        for(int i = 0; i < numberClass; i++) {
-                maxNumberClassInfo = numberClassInfo()[i];
-        }
-        return maxNumberClassInfo;
-    }
-    int [] numberClassAttributes() {
-    	int[] number = new int[r3.countClass(tmp)];
-    	number = r3.numberAttribute(tmp);
-    	return number;
-    }
-    int [] numberClassMethods() {
-    	int[] number = new int[r3.countClass(tmp)];
-    	number = r3.numberMethod(tmp);
-    	return number;
-    }
-    int [] numberClassInfo() {
         int [] numberClassInfo = new int[numberClass];
         for(int i = 0; i < numberClass; i++) {
-            numberClassInfo[i] = numberClassAttributes()[i] + numberClassMethods()[i] + 1;
+            numberClassInfo[i] = numberClassAttributes[i] + numberClassMethods[i] + 1;
         }
-        return numberClassInfo;	
-    }
-    int [] longestStringLen() {
+        this.numberClassInfo = numberClassInfo;
+        
+        int maxNumberInfo = 0;
+        for(int i = 0; i < numberClass; i++) {
+            if(numberClassInfo[i] > maxNumberInfo)
+                maxNumberInfo = numberClassInfo[i];
+        }
+        this.maxNumberInfo = maxNumberInfo;
+        
+        int maxNumberClassInfo = 0;
+        for(int i = 0; i < numberClass; i++) {
+                maxNumberClassInfo = numberClassInfo[i];
+        }
+        this.maxNumberClassInfo = maxNumberClassInfo;
+   
+        String[][] getInfo = new String[maxNumberInfo][maxNumberInfo];
+        getInfo = r3.getAll(tmp);
+        this.getInfo = getInfo;
+        
         int [] longestStringLen = new int[numberClass];
         for(int i = 0; i < numberClass; i++) {
             longestStringLen[i] = 0;
-            for(int j = 0; j < numberClassInfo()[i]; j++) {
-                if(getInfo()[i][j].length() > longestStringLen[i])
-                    longestStringLen[i] = getInfo()[i][j].length();
+            for(int j = 0; j < numberClassInfo[i]; j++) {
+                if(getInfo[i][j].length() > longestStringLen[i])
+                    longestStringLen[i] = getInfo[i][j].length();
             }
         }
-        return longestStringLen;
-    }
-    int [][] relationship() {
-        // 0 : no relationship
-        // 1 : generalization
-        // 2 : realization / implementation
-        // 3 : association
+        this.longestStringLen = longestStringLen;
+        
+        int relationship[][] = new int[100][100];
+        relationship = r3.getRelationship(tmp2);
+        this.relationship = relationship;
 
-    	int r[][] = new int[100][100];
-    	r = r3.getRelationship(tmp2);
-        return r;
-    }
-    String[][] getInfo() {
-		readFile2 r3 = new readFile2();
-		String tmp = r3.docFile("./diagram.txt");
-        String[][] getInfo = new String[maxNumberInfo()][maxNumberInfo()];
-        getInfo = r3.getAll(tmp);
-        return getInfo;
     }
 }
